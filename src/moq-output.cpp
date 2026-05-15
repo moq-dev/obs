d37e6d218a1fe8246a51d15a81b5ca6715e76f0f
Dan Rossi <electroteque@protonmail.com>
2026-05-15 22:36:08 +1000

    - fix pointer reference

--- src/moq-output.cpp
+++ src/moq-output.cpp
@@ -146,7 +146,7 @@ void MoQOutput::Stop(bool signal)
 		obs_output_signal_stop(output, OBS_OUTPUT_SUCCESS);
 	}
 
-	os_atomic_set_bool(&self->reconnecting, false);
+	os_atomic_set_bool(&reconnecting, false);
 
 	return;
 }
